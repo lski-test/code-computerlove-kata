53795525a87dc3f6efb8f306bd45c6d689eb242d
Lee Cooper <lee.cooper@lski.uk>
2019-03-10 16:09:07 +0000

    Create stubbed Checkout

--- src/Code.Checkout.Test/CheckoutTests.cs
+++ src/Code.Checkout.Test/CheckoutTests.cs
@@ -1,4 +1,4 @@
-using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Code.Checkout.Test
@@ -8,7 +8,9 @@ namespace Code.Checkout.Test
         [Fact]
         public void No_Items_Should_Be_Zero()
         {
-            throw new NotImplementedException();
+            var checkout = new Checkout();
+
+            checkout.GetTotalPrice().Should().Be(0);
         }
     }
 }
---
+++ src/Code.Checkout/Checkout.cs
@@ -0,0 +1,15 @@
+﻿namespace Code.Checkout
+{
+    public class Checkout : ICheckout
+    {
+        public decimal GetTotalPrice()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Scan(string item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
