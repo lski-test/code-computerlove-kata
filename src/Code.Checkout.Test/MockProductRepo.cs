3bd2605379db1b00fac3543dece809be242b1ce8
Lee Cooper <lee.cooper@lski.uk>
2019-03-11 18:15:26 +0000

    Add mocked data to the mocked product repo
    - Added an explanation of the GetBySku return type.

--- src/Code.Checkout.Test/MockProductRepo.cs
+++ src/Code.Checkout.Test/MockProductRepo.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Code.Checkout.Test
 {
+
+    /// <summary>
+    /// As this is an interview example then the repo wouldnt be hard coded like this, hence only creating a mocked repo.
+    /// </summary>
     public class MockProductRepo : IProductRepo
     {
+        private List<Product> _items;
+
+        public MockProductRepo()
+        {
+            _items = new List<Product> {
+                new Product { Sku = "A", Price = 50 },
+                new Product { Sku = "B", Price = 30 },
+                new Product { Sku = "C", Price = 20 },
+                new Product { Sku = "D", Price = 15 }
+            };
+        }
+
         public Product GetBySku(string sku)
         {
-            throw new NotImplementedException();
+            return _items.Find(i => i.Sku.Equals(sku, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
--- src/Code.Checkout/IProductRepo.cs
+++ src/Code.Checkout/IProductRepo.cs
@@ -2,6 +2,11 @@
 {
     public interface IProductRepo
     {
+        /// <summary>
+        /// Returns the product that matches the passed sku number.
+        /// </summary>
+        /// <param name="sku">The stock identifier code</param>
+        /// <returns>The product matching the Sku or null if not found</returns>
         Product GetBySku(string sku);
     }
 }
