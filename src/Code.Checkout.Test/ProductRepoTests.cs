9a67cd936f1562e5288585e56ca6f3b56a5a7b8d
Lee Cooper <lee.cooper@lski.uk>
2019-03-11 18:14:17 +0000

    Create tests for creating the mock version of product repo

---
+++ src/Code.Checkout.Test/ProductRepoTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Code.Checkout.Test
+{
+    /// <summary>
+    /// Its arguable whether you would test the mock, being as its a simple implementation. But useful for developing the mock itself.
+    /// </summary>
+    public class ProductRepoTests
+    {
+        [Fact]
+        public void Check_Correct_Products_Returned()
+        {
+            var productRepo = new MockProductRepo();
+
+            productRepo.GetBySku("A").Should().NotBeNull().And.Match<Product>(p => p.Price == 50);
+            productRepo.GetBySku("B").Should().NotBeNull().And.Match<Product>(p => p.Price == 30);
+            productRepo.GetBySku("C").Should().NotBeNull().And.Match<Product>(p => p.Price == 20);
+            productRepo.GetBySku("D").Should().NotBeNull().And.Match<Product>(p => p.Price == 15);
+
+            productRepo.GetBySku(null).Should().BeNull();
+            productRepo.GetBySku("blah").Should().BeNull();
+        }
+    }
+}
