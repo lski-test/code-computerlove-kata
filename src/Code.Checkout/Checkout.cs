e062b9ad3875250056754c6c97fc0689f34b9def
Lee Cooper <lee.cooper@lski.uk>
2019-03-11 16:25:29 +0000

    Add internal scanned item list

--- src/Code.Checkout/Checkout.cs
+++ src/Code.Checkout/Checkout.cs
@@ -1,7 +1,17 @@
-﻿namespace Code.Checkout
+﻿using System.Collections.Generic;
+
+namespace Code.Checkout
 {
     public class Checkout : ICheckout
     {
+        // NB: When not passing items as interfaces, use concrete classes as it reduces virtual calls in the compiler
+        private readonly List<Product> _items;
+
+        public Checkout()
+        {
+            _items = new List<Product>();
+        }
+
         public decimal TotalPrice { get; private set; }
 
         public void Scan(string item)
---
+++ src/Code.Checkout/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Code.Checkout
+{
+    public class Product
+    {
+        public string Sku { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
