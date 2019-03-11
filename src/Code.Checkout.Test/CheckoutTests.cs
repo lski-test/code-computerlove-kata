dea819705e6271480ec9a63d8abe0e0557045c5b
Lee Cooper <lee.cooper@lski.uk>
2019-03-11 18:21:35 +0000

    Create incorrect sku tests

--- src/Code.Checkout.Test/CheckoutTests.cs
+++ src/Code.Checkout.Test/CheckoutTests.cs
@@ -33,5 +33,27 @@ namespace Code.Checkout.Test
 
             checkout.TotalPrice.Should().Be(80);
         }
+
+        [Fact]
+        public void Multiple_Items_With_Incorrect_Sku_Should_Have_Correct_Total()
+        {
+            var checkout = new Checkout(new MockProductRepo());
+
+            checkout.Scan("A");
+            checkout.Scan("unknown");
+            checkout.Scan("B");
+
+            checkout.TotalPrice.Should().Be(80);
+        }
+
+        [Fact]
+        public void Scan_With_Incorrect_Sku_Should_Feedback_Correctly()
+        {
+            var checkout = new Checkout(new MockProductRepo());
+
+            checkout.Scan("A").Should().BeTrue();
+            checkout.Scan("unknown").Should().BeFalse();
+            checkout.Scan("B").Should().BeTrue();
+        }
     }
 }
