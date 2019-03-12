9d949afda98f16a76b3129af5f383c988977a403
Lee Cooper <lee.cooper@lski.uk>
2019-03-12 16:21:34 +0000

    Clean commented code
    (Only included to show I thought process for example)

--- src/Code.Checkout/Offers/MultipleItemsMatcher.cs
+++ src/Code.Checkout/Offers/MultipleItemsMatcher.cs
@@ -6,7 +6,7 @@ namespace Code.Checkout.Offers
 {
     public class MultipleItemsMatcher : IOfferMatcher
     {
-        private IEnumerable<string> _skus;
+        private IReadOnlyList<string> _skus;
         private readonly decimal _fixedPrice;
 
         public int Priority { get; }
@@ -22,32 +22,6 @@ namespace Code.Checkout.Offers
 
         public IEnumerable<IPriceModifer> Match(IReadOnlyList<CheckoutItem> items)
         {
-            //var itemSearchStartPos = 0;
-
-            //foreach (var item in items)
-            //{
-            //    var
-
-
-            //}
-
-            //for (int i = 0, n = items.Count(); i < n; i++)
-            //{
-
-            //    var item = items[0];
-
-            //    if (sku.Equals(item.Sku, StringComparison.OrdinalIgnoreCase))
-            //    {
-            //        matchedItems.Add(item);
-            //        itemSearchStartPos = i + 1;
-            //        break;
-            //    }
-            //}
-
-
-            // need to know at the end is 'next position in items'
-
-
             var itemSearchStartPos = 0;
             var offers = new List<IPriceModifer>();
 
@@ -62,36 +36,6 @@ namespace Code.Checkout.Offers
             }
 
             return offers;
-
-            //var matchedItems = new List<CheckoutItem>();
-            //var parsedItems = items.Where(i => !i.InDeal);
-
-            //foreach (var sku in _skus)
-            //{
-            //    for (int i = itemSearchStartPos, n = items.Count(); i < n; i++) {
-
-            //        var item = items[0];
-
-            //        if (sku.Equals(item.Sku, StringComparison.OrdinalIgnoreCase)) {
-            //            matchedItems.Add(item);
-            //            itemSearchStartPos = i + 1;
-            //            break;
-            //        }
-            //    }
-            //}
-
-            //// If the whole deal is complete then return the deal modifier
-            //// NB: Need to modify the items so they are ignored in future checks
-            //if (matchedItems.Count() == _skus.Count()) {
-            //    // TODO: hould be checking for more items that match the deal
-            //    return new[] {
-            //        new MultipleItemsPriceModifier {
-
-            //        }
-            //    };
-            //}
-
-            //return new MultipleItemsPriceModifier[] { };
 
             MultipleItemsPriceModifier ParseOffer()
             {
@@ -116,8 +60,9 @@ namespace Code.Checkout.Offers
 
                 // If the whole deal is complete then return the deal modifier
                 // NB: Need to modify the items so they are ignored in future checks
-                if (matchedItems.Count() == _skus.Count())
+                if (matchedItems.Count == _skus.Count)
                 {
+                    matchedItems.ForEach(i => i.InDeal = true);
                     // TODO: hould be checking for more items that match the deal
                     return
                         new MultipleItemsPriceModifier {
