c3ec138cbf1bb08c3a91959b9f85157b1e57498f
Lee Cooper <lee.cooper@lski.uk>
2019-03-13 09:17:06 +0000

    Create FindIndex function for ReadOnlyList
    Separate project as these are reusable components. In folder in case splitting desired later.
    
    I havent micro committed this, but took code from other work I have done in the past
    In that case I created the stubbed extensions and then the tests before filling out

---
+++ src/Code.Utils.Test/CollectionsTest.cs
@@ -0,0 +1,50 @@
+using Code.Utils.Collections;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Code.Utils.Test
+{
+    public class CollectionsTest
+    {
+        [Fact]
+        public void Find_Existing_Item()
+        {
+            IReadOnlyList<string> arr = new[] { "A", "B", "C" };
+
+            arr.FindIndex(item => item == "A").Should().Be(0);
+        }
+
+        [Fact]
+        public void Return_Minus_One_On_Not_Found()
+        {
+            IReadOnlyList<string> arr = new[] { "A", "B", "C" };
+
+            arr.FindIndex(item => item == "D").Should().Be(-1);
+        }
+
+        [Fact]
+        public void Find_Existing_Item_With_Idx()
+        {
+            IReadOnlyList<string> arr = new[] { "A", "B", "C" };
+
+            arr.FindIndex((item, idx) => {
+                if (item == "B")
+                {
+                    idx.Should().Be(1);
+                    return true;
+                }
+                return false;
+            }).Should().Be(1);
+        }
+
+        [Fact]
+        public void Return_Minus_One_On_Not_Found_With_Idx()
+        {
+            IReadOnlyList<string> arr = new[] { "A", "B", "C" };
+
+            arr.FindIndex((item, idx) => item == "D").Should().Be(-1);
+        }
+    }
+}
---
+++ src/Code.Utils/Collections/IReadOnlyExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code.Utils.Collections
+{
+    public static class IReadOnlyExtensions
+    {
+        public static int FindIndex<T>(this IReadOnlyList<T> list, Func<T, bool> predicate, int start = 0)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            for (int i = start, n = list.Count; i < n; i++)
+            {
+                if (predicate.Invoke(list[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int FindIndex<T>(this IReadOnlyList<T> list, Func<T, int, bool> predicate, int start = 0)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            for (int i = start, n = list.Count; i < n; i++)
+            {
+                if (predicate.Invoke(list[i], i))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
